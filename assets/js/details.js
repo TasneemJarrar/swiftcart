b4cbd31a89d2dfa36135b8a68f06b31d228f7fb0
Tasneem_jarrar <tasneem.a.jarrar@gmail.com>
2026-01-08 03:45:59 +0200

    comment section, pagination

--- assets/js/details.js
+++ assets/js/details.js
@@ -11,31 +11,34 @@ getProduct();
 // swiper
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
-  spaceBetween:30,
+  spaceBetween: 30,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-  loop : true,
-  effect: 'coverflow',
+  loop: true,
+  effect: "coverflow",
   coverflowEffect: {
     rotate: 30,
     slideShadows: false,
   },
-
 });
 
+// display Product Data
 const displayProductData = async () => {
-  // try{
+  try {
     const response = await getProduct();
-console.log(response);
-  const ProductImages = response.images.map(img => {
-    return `
-      <div class="swiper-slide w-100 rounded-5"><img src="${img}" alt="" class="w-100"></div>
-    `;
-  }).join('');
+    console.log(response);
 
-  const ProductInfo =  `
+    const ProductImages = response.images
+      .map((img) => {
+        return `
+        <div class="swiper-slide w-100 rounded-5"><img src="${img}" alt="" class="w-100"></div>
+      `;
+      })
+      .join("");
+
+    const ProductInfo = `
       <div class="text-information d-flex flex-column gap-3 align-items-start">
         <span class="emerald-badge px-2 fs-14 text-uppercase">${response.category}</span>
         <h2 class="fw-bold fs-1">${response.title}</h2>
@@ -46,23 +49,32 @@ console.log(response);
       </div>
     `;
 
-    document.querySelector(".product-images .swiper .swiper-wrapper").innerHTML = ProductImages;
+    const ProductRevies = response.reviews
+      .map((review) => {
+        const stars = "★".repeat(review.rating) + "☆".repeat(5 - review.rating);        
+        return `
+          <div class="comment-card card p-3 d-flex flex-column gap-3 align-items-start rounded-4 border-0">
+            <p class="fw-bold">${review.reviewerName}</p>
+            <span class="">${stars}</span>
+            <p class="fs-14">"${review.comment}"</p>
+          </div>
+      `;
+      })
+      .join("");
+
+    document.querySelector(
+      ".product-images .swiper .swiper-wrapper"
+    ).innerHTML = ProductImages;
     document.querySelector(".product .product-info").innerHTML = ProductInfo;
+    document.querySelector(".product .product-reviews .cards").innerHTML = ProductRevies;
 
-  //   document.querySelector(".user .details .userEmail").textContent = response.data.email;
-  //   document.querySelector(".user .details .userAge").textContent = response.data.age;
-  //   if (response.data.image) {
-  //     document.querySelector(".user .details .userImg").src = response.data.image;
-  //   }
-  //   document.querySelector(".loader").classList.add("d-none");
-
-  // } catch (error) {
-  //   document.querySelector(".loader").classList.remove("d-none");
-  //   document.querySelector(".loader").classList.add("d-none");
-  // } finally {
-  //   document.querySelector(".loader").classList.add("d-none");
-  // }
+    document.querySelector(".loader").classList.add("d-none");
+  } catch (error) {
+    document.querySelector(".loader").classList.remove("d-none");
+    document.querySelector(".loader").classList.add("d-none");
+  } finally {
+    document.querySelector(".loader").classList.add("d-none");
+  }
 };
+
 displayProductData();
-
-
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-const getProducts = async () => {
-  const response = await axios.get(`https://dummyjson.com/products`);
+const getProducts = async (page) => {
+  const skip = (page - 1) * 12;
+  console.log("skip: " + skip);
+
+  const response = await axios.get(
+    `https://dummyjson.com/products?limit=12&skip=${skip}`
+  );
   return response.data;
 };
 
 // display Bestsellers
 displayBestsellers = async () => {
-  const result = await getProducts();
+  const result = await getProducts(1);
   const lowestProduct = result.products.reduce((min, product) => {
     return product.price < min.price ? product : min;
   });
@@ -52,13 +57,12 @@ displayCatigories = async () => {
 };
 
 // display all products
-const displayallproducts = async () => {  
-  const result = await getProducts();
-  const numberOfPages = Math.ceil(result.total / 10);
-
-  console.log(numberOfPages);
-  const allproducts = result.products.map(product => {
-    return `
+const displayallproducts = async (page = 1) => {
+  const result = await getProducts(page);
+  const numberOfPages = Math.ceil(result.total / 12);
+  const allproducts = result.products
+    .map((product) => {
+      return `
       <div class="col-md-3">
         <a href="./details.html?id=${product.id}" class="product-card card p-4 card rounded-5 mt-4 d-flex gap-3 ">
           <div class="product-image p-3 rounded-4 position-relative d-flex justify-content-center" >
@@ -74,28 +78,75 @@ const displayallproducts = async () => {
         </a>
       </div>
     `;
-  }).join('');
+    })
+    .join("");
+  document.querySelector(".products .row").innerHTML = allproducts;
 
-  document.querySelector('.products .row').innerHTML = allproducts;
+  // pagination link
+  let pagination = ``;
+
+  if (page > 1) {
+    pagination = `<li class="page-item"><button class="page-link rounded-circle d-flex justify-content-center align-items-center" onclick=displayallproducts(${
+      page - 1
+    })><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <g fill="none" fill-rule="evenodd">
+            <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+            <path fill="#6c757d" d="M8.293 12.707a1 1 0 0 1 0-1.414l5.657-5.657a1 1 0 1 1 1.414 1.414L10.414 12l4.95 4.95a1 1 0 0 1-1.414 1.414z" />
+          </g>
+        </svg></button></li>`;
+  } else {
+    pagination = `<li class="page-item"><button class="page-link rounded-circle d-flex justify-content-center align-items-center" disabled><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <g fill="none" fill-rule="evenodd">
+            <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+            <path fill="#6c757d" d="M8.293 12.707a1 1 0 0 1 0-1.414l5.657-5.657a1 1 0 1 1 1.414 1.414L10.414 12l4.95 4.95a1 1 0 0 1-1.414 1.414z" />
+          </g>
+        </svg></button></li>`;
+  }
+  for (let i = 1; i <= numberOfPages; i++) {
+    if (i === page) {
+      pagination += `<li class="page-item"><button class="page-link active page-link rounded-circle" onclick=displayallproducts(${i})>${i}</button></li>`;
+    } else {
+      pagination += `<li class="page-item"><button class="page-link rounded-circle" onclick=displayallproducts(${i})>${i}</button></li>`;
+    }
+  }
+if (page < numberOfPages) {
+  pagination += `<li class="page-item"><button class="page-link rounded-circle d-flex justify-content-center align-items-center" onclick=displayallproducts(${page + 1})><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <g fill="none" fill-rule="evenodd">
+  <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+  <path fill="#6c757d" d="M15.707 11.293a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 1 1-1.414-1.414l4.95-4.95l-4.95-4.95a1 1 0 0 1 1.414-1.414z" />
+  </g>
+  </svg></button></li>`;
+}
+else{
+  pagination += `<li class="page-item"><button class="page-link rounded-circle d-flex justify-content-center align-items-center" disabled><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <g fill="none" fill-rule="evenodd">
+  <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+  <path fill="#6c757d" d="M15.707 11.293a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 1 1-1.414-1.414l4.95-4.95l-4.95-4.95a1 1 0 0 1 1.414-1.414z" />
+  </g>
+  </svg></button></li>`;
+}
+
+document.querySelector(".pagination").innerHTML = pagination;
 };
 displayallproducts();
 
 // Function to filter products by category
 const filterByCategory = async (category) => {
-  let response = 'https://dummyjson.com/products';
+  let response = "https://dummyjson.com/products";
   if (category) {
     response = `https://dummyjson.com/products/category/${category}`;
-  }
-  else{
-    category='The Collection';
+  } else {
+    category = "The Collection";
   }
   const result = await axios.get(response);
   displaybyCategory(result.data.products, category);
 };
+
 // Function to display products
 const displaybyCategory = (products, category) => {
-  const productsbycategory = products.map(product => {
-    return `
+  const productsbycategory = products
+    .map((product) => {
+      return `
       <div class="col-md-3">
         <a href="./details.html?id=${product.id}" class="product-card card p-4 card rounded-5 mt-4 d-flex gap-3 ">
           <div class="product-image p-3 rounded-4 position-relative d-flex justify-content-center" >
@@ -111,8 +162,11 @@ const displaybyCategory = (products, category) => {
         </a>
       </div>
     `;
-  }).join('');
-  document.querySelector('.products .category').innerHTML = category;
-  document.querySelector('.products .row').innerHTML = productsbycategory;
+    })
+    .join("");
+  document.querySelector(".products .category").innerHTML = category;
+  document.querySelector(".products .row").innerHTML = productsbycategory;
 };
 displayCatigories();
+
+// Pagenation
