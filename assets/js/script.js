767aecddfaf8700294b602ac15c1097150ece8bb
Tasneem_jarrar <tasneem.a.jarrar@gmail.com>
2026-01-08 04:04:11 +0200

    fix pagination

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,8 @@ displayCatigories = async () => {
       .join("");
   document.querySelector(".swiper .swiper-wrapper").innerHTML = categories;
   swiper.update();
+
+  
 };
 
 // display all products
@@ -130,20 +132,24 @@ document.querySelector(".pagination").innerHTML = pagination;
 };
 displayallproducts();
 
-// Function to filter products by category
-const filterByCategory = async (category) => {
-  let response = "https://dummyjson.com/products";
+// filter products by category with pagination
+const filterByCategory = async (category, page = 1) => {
+  const skip = (page - 1) * 12;
+  let response = `https://dummyjson.com/products?limit=12&skip=${skip}`;
+  
   if (category) {
-    response = `https://dummyjson.com/products/category/${category}`;
+    response = `https://dummyjson.com/products/category/${category}?limit=12&skip=${skip}`;
   } else {
-    category = "The Collection";
+    category = 'The Collection';
   }
+  
   const result = await axios.get(response);
-  displaybyCategory(result.data.products, category);
+  displaybyCategory(result.data.products, result.data.total, category, page);
 };
 
-// Function to display products
-const displaybyCategory = (products, category) => {
+// display products by category
+const displaybyCategory = (products, total, category, page) => {
+  const numberOfPages = Math.ceil(total / 12);
   const productsbycategory = products
     .map((product) => {
       return `
@@ -164,8 +170,58 @@ const displaybyCategory = (products, category) => {
     `;
     })
     .join("");
-  document.querySelector(".products .category").innerHTML = category;
-  document.querySelector(".products .row").innerHTML = productsbycategory;
+    
+  document.querySelector('.products .category').innerHTML = category;
+  document.querySelector('.products .row').innerHTML = productsbycategory;
+  
+  if (numberOfPages <= 1) {
+    document.querySelector('.pagination').innerHTML = '';
+    return;
+  }
+    let pagination = '';
+  const categoryParam = category === 'The Collection' ? null : category;
+  
+  if (page > 1) {
+    pagination = `<li class="page-item"><button class="page-link rounded-circle d-flex justify-content-center align-items-center" onclick="filterByCategory('${categoryParam}', ${page - 1})"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <g fill="none" fill-rule="evenodd">
+            <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+            <path fill="#6c757d" d="M8.293 12.707a1 1 0 0 1 0-1.414l5.657-5.657a1 1 0 1 1 1.414 1.414L10.414 12l4.95 4.95a1 1 0 0 1-1.414 1.414z" />
+          </g>
+        </svg></button></li>`;
+  } else {
+    pagination = `<li class="page-item"><button class="page-link rounded-circle d-flex justify-content-center align-items-center" disabled><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <g fill="none" fill-rule="evenodd">
+            <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+            <path fill="#6c757d" d="M8.293 12.707a1 1 0 0 1 0-1.414l5.657-5.657a1 1 0 1 1 1.414 1.414L10.414 12l4.95 4.95a1 1 0 0 1-1.414 1.414z" />
+          </g>
+        </svg></button></li>`;
+  }
+  
+  for (let i = 1; i <= numberOfPages; i++) {
+    if (i === page) {
+      pagination += `<li class="page-item"><button class="page-link active rounded-circle" onclick="filterByCategory('${categoryParam}', ${i})">${i}</button></li>`;
+    } else {
+      pagination += `<li class="page-item"><button class="page-link rounded-circle" onclick="filterByCategory('${categoryParam}', ${i})">${i}</button></li>`;
+    }
+  }
+  
+  if (page < numberOfPages) {
+    pagination += `<li class="page-item"><button class="page-link rounded-circle d-flex justify-content-center align-items-center" onclick="filterByCategory('${categoryParam}', ${page + 1})"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <g fill="none" fill-rule="evenodd">
+  <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+  <path fill="#6c757d" d="M15.707 11.293a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 1 1-1.414-1.414l4.95-4.95l-4.95-4.95a1 1 0 0 1 1.414-1.414z" />
+  </g>
+  </svg></button></li>`;
+  } else {
+    pagination += `<li class="page-item"><button class="page-link rounded-circle d-flex justify-content-center align-items-center" disabled><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+  <g fill="none" fill-rule="evenodd">
+  <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+  <path fill="#6c757d" d="M15.707 11.293a1 1 0 0 1 0 1.414l-5.657 5.657a1 1 0 1 1-1.414-1.414l4.95-4.95l-4.95-4.95a1 1 0 0 1 1.414-1.414z" />
+  </g>
+  </svg></button></li>`;
+  }
+  
+  document.querySelector('.pagination').innerHTML = pagination;
 };
 displayCatigories();
 
